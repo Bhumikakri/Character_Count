428051c2dd3935a7769a1e5be600b64164076bbf
Bhumikakri <128302166+Bhumikakri@users.noreply.github.com>
2023-10-13 16:27:44 +0530

    real time

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let Area = document.querySelector("textarea");
  let total = document.getElementById("Total");
  let remain = document.getElementById("Remaining");
-
+let audio = document.querySelector("audio");
 
 Area.addEventListener("input", (event)=>{
     // console.log(e.target.value);
     // console.log(e.target.value.length);
-
+    audio.play();
     let countText = event.target.value.length;
     let remainText = 50-event.target.value.length;
     total.innerText = countText;
